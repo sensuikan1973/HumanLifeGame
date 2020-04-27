ddb6f80f418fc61c57fe0d4e752616f4edb29d4b
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-04-27 23:00:16 +0900

    fix widget name

---
+++ lib/domain/play_room/play_room.dart
@@ -0,0 +1,9 @@
+import 'package:HumanLifeGame/domain/play_room/player_action.dart';
+import 'package:flutter/material.dart';
+
+class PlayRoom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: PlayerAction(),
+      );
+}
