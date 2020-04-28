ad8fc89c6918a2eba937f87a6c9a79dbb9944aad
TomoyukiSugiyama <u3.gpw25@ezweb.ne.jp>
2020-04-28 23:56:57 +0900

    add dice (#18)
    * add dice
    
    * fix README
    
    * fixed

--- test/widget/player_action_test.dart
+++ test/widget/player_action_test.dart
@@ -1,12 +1,21 @@
+import 'package:HumanLifeGame/domain/play_room/dice_result.dart';
 import 'package:HumanLifeGame/domain/play_room/player_action.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:HumanLifeGame/human_life_game_app.dart';
 
 void main() {
-  testWidgets('player action test', (tester) async {
-    await tester.pumpWidget(HumanLifeGameApp());
-    await tester.pump();
-    expect(find.byType(PlayerAction), findsOneWidget);
-    expect(find.text('Start'), findsOneWidget);
+  group('PlayRoom test', () {
+    testWidgets('player action test', (tester) async {
+      await tester.pumpWidget(HumanLifeGameApp());
+      await tester.pump();
+      expect(find.byType(PlayerAction), findsOneWidget);
+      expect(find.text('Start'), findsOneWidget);
+    });
+    testWidgets('dice result test', (tester) async {
+      await tester.pumpWidget(HumanLifeGameApp());
+      await tester.pump();
+      expect(find.byType(DiceResult), findsOneWidget);
+      expect(find.text('2'), findsOneWidget);
+    });
   });
 }
