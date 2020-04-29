eea4452226622d25f0c2ca4fde2c9f9427812362
done <sensuikan1973@gmail.com>
2020-04-29 21:51:56 +0900

    Router (#30)

--- lib/human_life_game_app.dart
+++ lib/human_life_game_app.dart
@@ -1,26 +1,35 @@
-import 'package:HumanLifeGame/screens/play_room/play_room.dart';
+import 'package:HumanLifeGame/router.dart';
 import 'package:HumanLifeGame/i18n/i18n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 import 'i18n/i18n_delegate.dart';
 
 class HumanLifeGameApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        onGenerateTitle: (context) => I18n.of(context).appTitle,
-        localizationsDelegates: const [
-          I18nDelegate(),
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          Provider(create: (context) => Router()),
         ],
-        supportedLocales: const [Locale('en', 'US'), Locale('ja', 'JP')],
-        locale: const Locale('en'),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+        child: Consumer<Router>(
+          builder: (_, router, __) => MaterialApp(
+            onGenerateTitle: (context) => I18n.of(context).appTitle,
+            localizationsDelegates: const [
+              I18nDelegate(),
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [Locale('en', 'US'), Locale('ja', 'JP')],
+            locale: const Locale('en'),
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            initialRoute: router.initialRoute,
+            routes: router.routes,
+          ),
         ),
-        home: PlayRoom(),
       );
 }
