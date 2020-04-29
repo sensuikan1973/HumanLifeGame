12a4b4f640599fbe3a027a43631efd1b1234ae68
ShimizuNaoki <sensuikan1973@gmail.com>
2020-04-29 21:39:40 +0900

    Router

---
+++ lib/router.dart
@@ -0,0 +1,10 @@
+import 'package:HumanLifeGame/screens/play_room/play_room.dart';
+import 'package:flutter/material.dart';
+
+class Router {
+  final String initialRoute = '/';
+
+  final Map<String, WidgetBuilder> routes = {
+    '/': (context) => PlayRoom(),
+  };
+}
