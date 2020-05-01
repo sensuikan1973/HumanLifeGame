9a6bdd79b07ec396dc7031bda9a25b06838011ab
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-02 00:20:14 +0900

    define HumanLife Models

---
+++ lib/models/human_life.dart
@@ -0,0 +1,8 @@
+import 'package:HumanLifeGame/models/life_road.dart';
+import 'package:HumanLifeGame/models/user.dart';
+
+class HumanLife {
+  String title;
+  UserModel author;
+  LifeRoad lifeRoad;
+}
