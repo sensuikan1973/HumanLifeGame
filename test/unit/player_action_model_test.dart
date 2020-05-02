3264d5947f9b43d6cde5457e8587834407a28a6e
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-02 22:27:59 +0900

    Dice API test

--- test/unit/player_action_model_test.dart
+++ test/unit/player_action_model_test.dart
@@ -1,11 +1,25 @@
-import 'package:HumanLifeGame/models/player_action.dart';
+import 'package:HumanLifeGame/api/dice.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../mocks/mocks.dart';
 
 void main() {
-  group('PlayerActionModel', () {
-    test("Dice value shuld be '1 <= value <= 6')", () {
-      final model = PlayerActionModel()..rollDice();
-      expect(model.dice, inInclusiveRange(1, PlayerActionModel.maxDiceNumber));
+  group('Dice', () {
+    test('default: 1 <= roll <= 6 ', () {
+      const dice = Dice();
+      expect(dice.roll(), inInclusiveRange(Dice.defaultMin, Dice.defaultMax));
+    });
+
+    test('custom: 2 <= roll <= 4 ', () {
+      const dice = Dice(min: 2, max: 4);
+      expect(dice.roll(), inInclusiveRange(2, 4));
+    });
+
+    test('mock roll()', () {
+      final dice = MockDice();
+      when(dice.roll()).thenReturn(3);
+      expect(dice.roll(), 3);
     });
   });
 }
