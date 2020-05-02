f185927a4af77953735cf29fa17047db180b81a0
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-03 02:02:44 +0900

    roll announcement i18n

--- lib/i18n/extensions/i18n_play_room.dart
+++ lib/i18n/extensions/i18n_play_room.dart
@@ -2,7 +2,7 @@ import 'package:HumanLifeGame/i18n/i18n.dart';
 import 'package:intl/intl.dart';
 
 /// PlayRoom Locale Text
-extension PlayRoom on I18n {
+extension I18nPlayRoom on I18n {
   String get rollDice => Intl.message(
         'Roll the dice',
         name: 'rollDice',
@@ -11,6 +11,12 @@ extension PlayRoom on I18n {
   String get lifeEventRecordesText => Intl.message(
         'Reserved area:lifeEventRecords',
         name: 'lifeEventRecordesText',
+        locale: localeName,
+      );
+  String rollAnnouncement(int roll) => Intl.message(
+        'result: $roll',
+        name: 'rollAnnouncement',
+        args: [roll],
         locale: localeName,
       );
 }
