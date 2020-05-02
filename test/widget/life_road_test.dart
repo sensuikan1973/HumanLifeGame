381250f2ec9483dbad234ab83ddde1d994cf030a
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-02 21:14:49 +0900

    fix test

--- test/widget/life_road_test.dart
+++ test/widget/life_road_test.dart
@@ -1,30 +1,17 @@
-import 'package:HumanLifeGame/i18n/i18n_delegate.dart';
 import 'package:HumanLifeGame/screens/play_room/life_road.dart';
 import 'package:HumanLifeGame/screens/play_room/life_step.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+import 'helper/widget_build_helper.dart';
+
+Future<void> main() async {
+  const locale = Locale('en', 'US');
   group('LifeRoad', () {
     testWidgets('show LifeStep', (tester) async {
-      await tester.pumpWidget(_LifeRoad());
+      await tester.pumpWidget(testableApp(locale: locale, home: LifeRoad()));
       await tester.pump();
       expect(find.byType(LifeStep), findsOneWidget);
     });
   });
-}
-
-class _LifeRoad extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Test App For LifeRoad',
-        localizationsDelegates: const [I18nDelegate()],
-        supportedLocales: const [Locale('en', 'US')],
-        locale: const Locale('en'),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: LifeRoad(),
-      );
 }
--- test/widget/life_step_test.dart
+++ test/widget/life_step_test.dart
@@ -1,29 +1,16 @@
-import 'package:HumanLifeGame/i18n/i18n_delegate.dart';
 import 'package:HumanLifeGame/screens/play_room/life_step.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+import 'helper/widget_build_helper.dart';
+
+Future<void> main() async {
+  const locale = Locale('en', 'US');
   group('LifeStep', () {
     testWidgets('show DecoratedBox', (tester) async {
-      await tester.pumpWidget(_LifeStep());
+      await tester.pumpWidget(testableApp(locale: locale, home: LifeStep()));
       await tester.pump();
       expect(find.byType(DecoratedBox), findsOneWidget);
     });
   });
-}
-
-class _LifeStep extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Test App For LifeStep',
-        localizationsDelegates: const [I18nDelegate()],
-        supportedLocales: const [Locale('en', 'US')],
-        locale: const Locale('en'),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: LifeStep(),
-      );
 }
