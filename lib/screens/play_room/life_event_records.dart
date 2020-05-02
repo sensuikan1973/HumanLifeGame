22a06e09d15c608e9581260587d44cbc0b6427ee
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-02 21:46:46 +0900

    add life event records widget

---
+++ lib/screens/play_room/life_event_records.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LifeEventRecordes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => const SizedBox(
+        width: 400,
+        height: 100,
+        child: Text(
+          'Human 1が２マス進みました。',
+        ),
+      );
+}
