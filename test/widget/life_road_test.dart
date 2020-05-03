ac9e855c7de6b8a7ef1183505373c1f864a4ad86
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-03 23:54:36 +0900

    delete unused code

--- test/widget/life_road_test.dart
+++ test/widget/life_road_test.dart
@@ -1,13 +1,7 @@
-import 'package:HumanLifeGame/i18n/i18n.dart';
-import 'package:HumanLifeGame/i18n/i18n_delegate.dart';
-import 'package:HumanLifeGame/models/play_room.dart';
-import 'package:HumanLifeGame/router.dart';
 import 'package:HumanLifeGame/screens/common/life_road.dart';
 import 'package:HumanLifeGame/screens/common/life_step.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 
 import 'helper/widget_build_helper.dart';
 
