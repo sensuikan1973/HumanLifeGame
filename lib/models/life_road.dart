e2080dd83856a6db066b7cba3296d4ecd10213c2
done <sensuikan1973@gmail.com>
2020-05-03 17:29:21 +0900

    temporary humanLifeRepository implement (#50)
    * temporary humanLifeRepository implement
    
    * define Infra class
    
    * fix LifeRoadModel

--- lib/models/life_road.dart
+++ lib/models/life_road.dart
@@ -1,7 +1,17 @@
 import 'package:HumanLifeGame/models/life_step.dart';
+import 'package:flutter/foundation.dart';
 
 class LifeRoadModel {
-  LifeRoadModel(this.lifeStepsOnBoard);
+  // FIXME: いつか消す
+  LifeRoadModel.dummy() {
+    // TODO: lifeStepsOnBoard に LifeStep をいくつか雑に入れておく
+  }
 
-  final List<List<LifeStepModel>> lifeStepsOnBoard;
+  @visibleForTesting
+  static const int width = 10;
+
+  @visibleForTesting
+  static const int height = 10;
+
+  List<List<LifeStepModel>> lifeStepsOnBoard = List.generate(width, (index) => List.generate(height, (index) => null));
 }
