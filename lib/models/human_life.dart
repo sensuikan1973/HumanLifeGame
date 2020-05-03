8483da6a940b7a0a9b89c48f1f9be8a8346702e1
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-03 15:01:51 +0900

    add dummyLifeRoad

--- lib/models/human_life.dart
+++ lib/models/human_life.dart
@@ -1,8 +1,25 @@
 import 'package:HumanLifeGame/models/life_road.dart';
+import 'package:HumanLifeGame/models/life_step.dart';
 import 'package:HumanLifeGame/models/user.dart';
 
 class HumanLifeModel {
+  HumanLifeModel(this.title, this.author, this.lifeRoad);
+  HumanLifeModel.dummy() {
+    title = 'dummyTytle';
+    lifeRoad = setDummyLifeRoad;
+  }
+
   String title;
   UserModel author;
   LifeRoadModel lifeRoad;
+
+  LifeRoadModel get setDummyLifeRoad {
+    LifeRoadModel dummy;
+    List<List<LifeStepModel>> lifeStepsOnBoard;
+    LifeStepModel lifeStep;
+    lifeStepsOnBoard = List.generate(10, (index) => List.generate(10, (index) => null));
+    lifeStepsOnBoard[5][5] = lifeStep;
+    dummy.lifeStepsOnBoard = lifeStepsOnBoard;
+    return dummy;
+  }
 }
--- lib/models/life_road.dart
+++ lib/models/life_road.dart
@@ -3,5 +3,5 @@ import 'package:HumanLifeGame/models/life_step.dart';
 class LifeRoadModel {
   LifeRoadModel(this.lifeStepsOnBoard);
 
-  final List<List<LifeStepModel>> lifeStepsOnBoard;
+  List<List<LifeStepModel>> lifeStepsOnBoard;
 }
