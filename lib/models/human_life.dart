ee7f17aab06cd9f246044b2d75c11fcd8056e2bc
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-03 18:57:56 +0900

    fix indent

--- lib/models/human_life.dart
+++ lib/models/human_life.dart
@@ -6,18 +6,25 @@ class HumanLifeModel {
   HumanLifeModel(this.title, this.author, this.lifeRoad);
   HumanLifeModel.dummy() {
     title = 'dummyTytle';
-    lifeRoad = setDummyLifeRoad;
+    lifeRoad = _setDummyLifeRoad;
   }
 
   String title;
   UserModel author;
   LifeRoadModel lifeRoad;
 
-  LifeRoadModel get setDummyLifeRoad {
+  LifeRoadModel get _setDummyLifeRoad {
     LifeRoadModel dummy;
     List<List<LifeStepModel>> lifeStepsOnBoard;
     LifeStepModel lifeStep;
-    lifeStepsOnBoard = List.generate(10, (index) => List.generate(10, (index) => null));
+
+    lifeStepsOnBoard = List.generate(
+      10,
+      (index) => List.generate(
+        10,
+        (index) => null,
+      ),
+    );
     lifeStepsOnBoard[5][5] = lifeStep;
     dummy.lifeStepsOnBoard = lifeStepsOnBoard;
     return dummy;
