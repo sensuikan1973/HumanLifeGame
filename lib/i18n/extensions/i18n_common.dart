1d999bdb96ebc7d1384818516b1d4f80f0688e3d
TomoyukiSugiyama <u3.gpw25@ezweb.ne.jp>
2020-05-05 05:43:36 +0900

    Eventのメッセージを追加 (#62)
    * add buildEventMessage
    
    * add tests for LiveEvents
    
    * add id
    
    * use Intl.select
    
    * delete unsued_import
    
    * delete blank

--- lib/i18n/extensions/i18n_common.dart
+++ lib/i18n/extensions/i18n_common.dart
@@ -1,4 +1,5 @@
 import 'package:HumanLifeGame/i18n/i18n.dart';
+import 'package:HumanLifeGame/models/common/life_event.dart';
 import 'package:intl/intl.dart';
 
 /// Common Locale Text
@@ -6,6 +7,18 @@ extension I18nCommon on I18n {
   String get appTitle => Intl.message(
         'Human Life Game',
         name: 'appTitle',
+        locale: localeName,
+      );
+  String lifeStepEventType(LifeEventType type) => Intl.select(
+        type,
+        {
+          LifeEventType.nothing: '',
+          LifeEventType.start: 'Start',
+          LifeEventType.goal: 'Goal',
+          LifeEventType.gainLifeItem: 'Gain Item :',
+        },
+        name: 'lifeStepEventType',
+        args: [type],
         locale: localeName,
       );
 }
