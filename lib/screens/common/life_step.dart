2ab34c7698b575ac01ec1946527604ed14c897eb
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-05 01:08:06 +0900

    change operator

--- lib/screens/common/life_step.dart
+++ lib/screens/common/life_step.dart
@@ -18,43 +18,47 @@ class LifeStep extends StatelessWidget {
   static Color exist = Colors.cyan[50];
 
   @override
-  Widget build(BuildContext context) => model.isStart == true
-      ? Stack(
-          children: <Widget>[
-            SizedBox(
-              width: width / LifeRoadModel.width,
-              height: height / LifeRoadModel.height,
-              child: Padding(
-                padding: const EdgeInsets.all(2),
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
-                  ),
+  Widget build(BuildContext context) {
+    if (model.isStart == true) {
+      return Stack(
+        children: <Widget>[
+          SizedBox(
+            width: width / LifeRoadModel.width,
+            height: height / LifeRoadModel.height,
+            child: Padding(
+              padding: const EdgeInsets.all(2),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
                 ),
               ),
             ),
-            const Positioned(
-              top: 0,
-              left: 0,
-              child: Human(),
-            ),
-            const Positioned(
-              bottom: 0,
-              left: 0,
-              child: Human(),
-            ),
-          ],
-        )
-      : SizedBox(
-          width: width / LifeRoadModel.width,
-          height: height / LifeRoadModel.height,
-          child: Padding(
-            padding: const EdgeInsets.all(2),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
-              ),
+          ),
+          const Positioned(
+            top: 0,
+            left: 0,
+            child: Human(),
+          ),
+          const Positioned(
+            bottom: 0,
+            left: 0,
+            child: Human(),
+          ),
+        ],
+      );
+    } else {
+      return SizedBox(
+        width: width / LifeRoadModel.width,
+        height: height / LifeRoadModel.height,
+        child: Padding(
+          padding: const EdgeInsets.all(2),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
             ),
           ),
-        );
+        ),
+      );
+    }
+  }
 }
