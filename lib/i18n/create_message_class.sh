614a16f10d0ea84166e954ef08d5186f3c9c1c69
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-04 19:22:13 +0900

    add comment

--- lib/i18n/create_message_class.sh
+++ lib/i18n/create_message_class.sh
@@ -5,5 +5,7 @@ flutter pub run intl_translation:generate_from_arb \
 lib/i18n/i18n.dart lib/i18n/extensions/*.dart \
 lib/i18n/intl_*.arb
 
-# 本当は exclude とかしたい
+# 本当は exclude option 的なのがあれば使いたいけど、無い。
+# See: https://github.com/dart-lang/dart_style/issues/864#issuecomment-544628480
+# ↑みたくやってもいいけど、まあ format だけだしここに入れちゃってる
 flutter format ./ -l 120
