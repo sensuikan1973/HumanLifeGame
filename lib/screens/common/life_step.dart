ba448096e49f2226f59df0a4e9558f4be3793a43
TomoyukiSugiyama <u3.gpw25@ezweb.ne.jp>
2020-05-05 02:00:17 +0900

    スタートの位置にのみHumanを表示 (#60)
    * add human
    
    * change human size&color
    
    * change operator
    
    * create method
    
    * fix method

--- lib/screens/common/life_step.dart
+++ lib/screens/common/life_step.dart
@@ -1,12 +1,13 @@
 import 'package:HumanLifeGame/models/common/life_event.dart';
 import 'package:HumanLifeGame/models/common/life_road.dart';
+import 'package:HumanLifeGame/models/common/life_step.dart';
 import 'package:HumanLifeGame/screens/play_room/human.dart';
 import 'package:flutter/material.dart';
 
 class LifeStep extends StatelessWidget {
   const LifeStep(this.model, this.width, this.height);
 
-  final LifeEventModel model;
+  final LifeStepModel model;
   final double width;
   final double height;
 
@@ -17,7 +18,24 @@ class LifeStep extends StatelessWidget {
   static Color exist = Colors.cyan[50];
 
   @override
-  Widget build(BuildContext context) => Stack(
+  Widget build(BuildContext context) {
+    if (model.isStart == true) return _squareWithHuman();
+
+    return SizedBox(
+      width: width / LifeRoadModel.width,
+      height: height / LifeRoadModel.height,
+      child: Padding(
+        padding: const EdgeInsets.all(2),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _squareWithHuman() => Stack(
         children: <Widget>[
           SizedBox(
             width: width / LifeRoadModel.width,
@@ -26,14 +44,19 @@ class LifeStep extends StatelessWidget {
               padding: const EdgeInsets.all(2),
               child: DecoratedBox(
                 decoration: BoxDecoration(
-                  color: model.type == LifeEventType.nothing ? nothing : exist,
+                  color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
                 ),
               ),
             ),
           ),
           const Positioned(
             top: 0,
-            right: 0,
+            left: 0,
+            child: Human(),
+          ),
+          const Positioned(
+            bottom: 0,
+            left: 0,
             child: Human(),
           ),
         ],
