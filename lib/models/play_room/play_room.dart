0ce8f27be4b2a4fad64f58e18420c7710af8b40b
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-04 20:49:37 +0900

    fix bug

--- lib/models/play_room/play_room.dart
+++ lib/models/play_room/play_room.dart
@@ -32,7 +32,7 @@ class PlayRoomModel extends ChangeNotifier {
       announcement.message = _i18n.rollAnnouncement(_currentPlayer.name, playerAction.roll);
       // FIXME: 仮で即ターン交代してる
       final currentPlayerIndex = humans.indexOf(_currentPlayer);
-_currentPlayer = humans[ (currentPlayerIndex + 1) % humans.length ]
+      _currentPlayer = humans[(currentPlayerIndex + 1) % humans.length];
     } else {
       _currentPlayer = humans.first;
     }
