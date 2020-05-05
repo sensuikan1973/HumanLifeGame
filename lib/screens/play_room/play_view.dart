6e79c30b0b0f8022ff02983f0b3644777156b135
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-05 19:08:58 +0900

    remove unnecessary Align

--- lib/screens/play_room/play_view.dart
+++ lib/screens/play_room/play_view.dart
@@ -8,18 +8,16 @@ class PlayView extends StatelessWidget {
   const PlayView({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Align(
-        child: SizedBox(
-          width: 1050,
-          height: 700,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.blue),
-            ),
-            child: Center(
-              child: LifeRoad(Provider.of<PlayRoomModel>(context, listen: false).humanLife.lifeRoad),
-            ),
+  Widget build(BuildContext context) => SizedBox(
+        width: 1050,
+        height: 700,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.blue),
+          ),
+          child: Center(
+            child: LifeRoad(Provider.of<PlayRoomModel>(context, listen: false).humanLife.lifeRoad),
           ),
         ),
       );
