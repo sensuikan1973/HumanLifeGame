2f8e17cf59e0029ad2146f32a69280ce68c775fb
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-05 19:55:52 +0900

    remove unnecessary method

--- lib/screens/common/life_step.dart
+++ lib/screens/common/life_step.dart
@@ -4,7 +4,6 @@ import '../../i18n/i18n.dart';
 import '../../models/common/life_event.dart';
 import '../../models/common/life_road.dart';
 import '../../models/common/life_step.dart';
-import '../play_room/human.dart';
 
 class LifeStep extends StatelessWidget {
   const LifeStep(
@@ -25,29 +24,7 @@ class LifeStep extends StatelessWidget {
   static Color exist = Colors.cyan[50];
 
   @override
-  Widget build(BuildContext context) {
-    if (model.lifeEvent.isStart) return _squareWithHuman(context);
-
-    return Stack(
-      children: <Widget>[
-        SizedBox(
-          width: width / LifeRoadModel.width,
-          height: height / LifeRoadModel.height,
-          child: Padding(
-            padding: const EdgeInsets.all(2),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                color: model.lifeEvent.type == LifeEventType.nothing ? nothing : exist,
-              ),
-            ),
-          ),
-        ),
-        Text(I18n.of(context).lifeStepEventType(model.lifeEvent.type)),
-      ],
-    );
-  }
-
-  Widget _squareWithHuman(BuildContext context) => Stack(
+  Widget build(BuildContext context) => Stack(
         children: <Widget>[
           SizedBox(
             width: width / LifeRoadModel.width,
@@ -62,16 +39,6 @@ class LifeStep extends StatelessWidget {
             ),
           ),
           Text(I18n.of(context).lifeStepEventType(model.lifeEvent.type)),
-          const Positioned(
-            top: 0,
-            left: 0,
-            child: Human(),
-          ),
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            child: Human(),
-          ),
         ],
       );
 }
