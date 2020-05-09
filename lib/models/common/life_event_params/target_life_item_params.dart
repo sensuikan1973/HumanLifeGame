0fb3d5718523e2b536c16759d9b64279872616f1
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-10 01:25:58 +0900

    fix metadata

--- lib/models/common/life_event_params/target_life_item_params.dart
+++ lib/models/common/life_event_params/target_life_item_params.dart
@@ -8,7 +8,11 @@ part 'target_life_item_params.g.dart';
 
 @freezed
 abstract class TargetLifeItemParams with _$TargetLifeItemParams {
-  const factory TargetLifeItemParams({@required String key, LifeItemType type, int amount}) = _TargetLifeItemParams;
+  const factory TargetLifeItemParams({
+    @required String key,
+    @required LifeItemType type,
+    @required int amount,
+  }) = _TargetLifeItemParams;
 
   factory TargetLifeItemParams.fromJson(Map<String, dynamic> json) => _$TargetLifeItemParamsFromJson(json);
 }
