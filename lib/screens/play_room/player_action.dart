b4998d0b0cd32f4bf12fdaa3ccd008e8d7b8a501
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-09 10:50:28 +0900

    add buttons

--- lib/screens/play_room/player_action.dart
+++ lib/screens/play_room/player_action.dart
@@ -8,20 +8,62 @@ class PlayerAction extends StatelessWidget {
   const PlayerAction({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          child: FlatButton(
-            key: const Key('playerActionDiceRollButton'),
-            color: Colors.blue,
-            textColor: Colors.white,
-            onPressed: () => context.read<PlayerActionModel>().rollDice(),
-            child: Text(
-              I18n.of(context).rollDice,
-              style: const TextStyle(fontSize: 20),
-            ),
-          ),
+  Widget build(BuildContext context) => Container(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: <Widget>[
+            _directionSelectButtons(context),
+            _twoChoiceButton(context),
+            _rollDiceButton(context),
+          ],
         ),
+      );
+
+  FlatButton _rollDiceButton(BuildContext context) => FlatButton(
+        key: const Key('playerActionDiceRollButton'),
+        color: Colors.blue,
+        textColor: Colors.white,
+        onPressed: () => context.read<PlayerActionModel>().rollDice(),
+        child: Text(
+          I18n.of(context).rollDice,
+          style: const TextStyle(fontSize: 20),
+        ),
+      );
+  Column _directionSelectButtons(BuildContext context) => Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              const Spacer(),
+              IconButton(icon: Icon(Icons.arrow_upward), onPressed: null),
+              const Spacer(),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Spacer(),
+              IconButton(icon: Icon(Icons.arrow_back), onPressed: null),
+              const Spacer(),
+              IconButton(icon: Icon(Icons.arrow_forward), onPressed: null),
+              const Spacer(),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Spacer(),
+              IconButton(icon: Icon(Icons.arrow_downward), onPressed: null),
+              const Spacer(),
+            ],
+          ),
+        ],
+      );
+  Row _twoChoiceButton(BuildContext context) => Row(
+        children: <Widget>[
+          const Spacer(),
+          IconButton(icon: Icon(Icons.close), onPressed: null),
+          const Spacer(),
+          IconButton(icon: Icon(Icons.radio_button_unchecked), onPressed: null),
+          const Spacer(),
+        ],
       );
 }
