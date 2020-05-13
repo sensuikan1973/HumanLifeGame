689e1fc61190ef7fcd9be0aecac34fc48faf0023
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-14 02:12:38 +0900

    add text in dialog

--- lib/screens/play_room/play_room.dart
+++ lib/screens/play_room/play_room.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../models/play_room/play_room.dart';
 import 'announcement.dart';
 import 'dice_result.dart';
 import 'life_event_records.dart';
@@ -11,24 +13,45 @@ class PlayRoom extends StatelessWidget {
   const PlayRoom({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Row(
-          children: <Widget>[
-            Column(
-              children: const <Widget>[
-                Announcement(),
-                LifeEventRecords(),
-                PlayView(),
-              ],
-            ),
-            Column(
-              children: const <Widget>[
-                LifeStages(),
-                DiceResult(),
-                PlayerAction(),
-              ],
-            ),
-          ],
-        ),
-      );
+  Widget build(BuildContext context) {
+    final _allHumansReachedTheGoal = context.select<PlayRoomModel, bool>((model) => model.allHumansReachedTheGoal);
+    if (_allHumansReachedTheGoal) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showDialog<void>(
+            context: context,
+            builder: (context) {
+              return const SimpleDialog(
+                title: Text('ok'),
+                children: <Widget>[
+                  Text('Human 1'),
+                  Text('Human 1'),
+                  Text('Human 1'),
+                  Text('Human 1'),
+                ],
+              );
+            });
+        return;
+      });
+    }
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          Column(
+            children: const <Widget>[
+              Announcement(),
+              LifeEventRecords(),
+              PlayView(),
+            ],
+          ),
+          Column(
+            children: const <Widget>[
+              LifeStages(),
+              DiceResult(),
+              PlayerAction(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
