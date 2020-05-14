16c304d734770584746bd52609e9058679ca0132
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-14 21:38:55 +0900

    add direction field

--- lib/models/play_room/player_action.dart
+++ lib/models/play_room/player_action.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/foundation.dart';
 
 import '../../api/dice.dart';
+import '../common/life_step.dart';
 
 class PlayerActionModel extends ChangeNotifier {
   PlayerActionModel(this._dice);
 
   final Dice _dice;
 
-  int _roll = 0; // 出目
+  /// 出目
   int get roll => _roll;
+  int _roll = 0;
 
-  // 一度も振られていない
+  /// 一度も振られていないかどうか
   bool get neverRolled => _roll == 0;
+
+  /// 方向の選択
+  Direction _direction;
+  Direction get direction => _direction;
+  set direction(Direction value) {
+    _direction = value;
+    notifyListeners();
+  }
 
   void rollDice() {
     _roll = _dice.roll();
