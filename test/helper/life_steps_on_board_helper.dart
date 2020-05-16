07227b612a7d24bb7f951eee9c29f5d759f37b1d
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-17 02:55:07 +0900

    minor fix

--- test/helper/life_steps_on_board_helper.dart
+++ test/helper/life_steps_on_board_helper.dart
@@ -6,7 +6,7 @@ import 'package:HumanLifeGame/models/common/life_step.dart';
 List<List<LifeStepModel>> createDummyLifeStepsOnBoard(List<List<LifeEventModel>> lifeEvents) => List.generate(
       lifeEvents.length,
       (y) => List.generate(
-        lifeEvents.first.length,
+        lifeEvents[y].length,
         (x) => LifeStepModel(
           id: x + (y * lifeEvents[y].length),
           lifeEvent: lifeEvents[y][x],
