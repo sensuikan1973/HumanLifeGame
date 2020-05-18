b454e8631cf50d14e90398359bdaa824cad07c58
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-18 16:44:30 +0900

    dummy replace with lifeEvents for test

--- test/widget/play_view_test.dart
+++ test/widget/play_view_test.dart
@@ -1,6 +1,11 @@
 import 'package:HumanLifeGame/i18n/i18n.dart';
 import 'package:HumanLifeGame/models/common/human.dart';
 import 'package:HumanLifeGame/models/common/human_life.dart';
+import 'package:HumanLifeGame/models/common/life_event.dart';
+import 'package:HumanLifeGame/models/common/life_event_params/gain_life_items_params.dart';
+import 'package:HumanLifeGame/models/common/life_event_params/goal_params.dart';
+import 'package:HumanLifeGame/models/common/life_event_params/nothing_params.dart';
+import 'package:HumanLifeGame/models/common/life_event_params/start_params.dart';
 import 'package:HumanLifeGame/models/common/life_road.dart';
 import 'package:HumanLifeGame/models/common/user.dart';
 import 'package:HumanLifeGame/models/play_room/play_room.dart';
@@ -14,34 +19,41 @@ import 'helper/widget_build_helper.dart';
 Future<void> main() async {
   final i18n = await I18n.load(const Locale('en', 'US'));
   final orderedHumans = [HumanModel(id: 'h1', name: 'foo'), HumanModel(id: 'h2', name: 'bar')];
+  final start = LifeEventModel(LifeEventTarget.myself, const StartParams());
+  final goals = LifeEventModel(LifeEventTarget.myself, const GoalParams());
+  final gains = LifeEventModel(LifeEventTarget.myself, const GainLifeItemsParams(targetItems: []));
+  final blank = LifeEventModel(LifeEventTarget.myself, const NothingParams());
+  final lifeEvents = [
+    [start, gains, gains, gains, gains, gains, goals],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+    [blank, blank, blank, blank, blank, blank, blank],
+  ];
   final humanLife = HumanLifeModel(
-    title: 'dummy HumanLife',
-    author: UserModel(id: 'dummyUserId', name: 'dummyUser'),
-    lifeRoad: LifeRoadModel(
-      lifeStepsOnBoard: LifeRoadModel.createLifeStepsOnBoard(
-        LifeRoadModel.dummyLifeEvents(),
-      ),
-    ),
+    title: 'hello',
+    author: UserModel(id: 'user', name: 'hoge'),
+    lifeRoad: LifeRoadModel(lifeStepsOnBoard: LifeRoadModel.createLifeStepsOnBoard(lifeEvents)),
   );
 
-  testWidgets('show LifeStep, display size = (1440, 1024)', (tester) async {
+  testWidgets('show LifeSteps, display size = (1440, 1024)', (tester) async {
     WidgetsBinding.instance.renderView.configuration = TestViewConfiguration(size: const Size(1440, 1024));
     final playRoomModel = PlayRoomNotifier(i18n, humanLife, orderedHumans: orderedHumans);
-    final widgets = playRoomModel.humanLife.lifeRoad.height * playRoomModel.humanLife.lifeRoad.width;
     await tester.pumpWidget(testableApp(
       home: ChangeNotifierProvider(create: (_) => playRoomModel, child: const PlayView()),
     ));
     await tester.pump();
-    expect(find.byType(LifeStep), findsNWidgets(widgets));
+    expect(find.byType(LifeStep), findsNWidgets(lifeEvents.length * lifeEvents.first.length));
   });
-  testWidgets('show LifeStep, display size = (800, 500)', (tester) async {
-    WidgetsBinding.instance.renderView.configuration = TestViewConfiguration(size: const Size(800, 500));
+  testWidgets('show LifeSteps, display size = (1050, 750)', (tester) async {
+    WidgetsBinding.instance.renderView.configuration = TestViewConfiguration(size: const Size(1050, 750));
     final playRoomModel = PlayRoomNotifier(i18n, humanLife, orderedHumans: orderedHumans);
-    final widgets = playRoomModel.humanLife.lifeRoad.height * playRoomModel.humanLife.lifeRoad.width;
     await tester.pumpWidget(testableApp(
       home: ChangeNotifierProvider(create: (_) => playRoomModel, child: const PlayView()),
     ));
     await tester.pump();
-    expect(find.byType(LifeStep), findsNWidgets(widgets));
+    expect(find.byType(LifeStep), findsNWidgets(lifeEvents.length * lifeEvents.first.length));
   });
 }
