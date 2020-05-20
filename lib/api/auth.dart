b626ef568d9ba911e8a9c591760f5cab5ffd37f1
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-20 21:33:54 +0900

    auth implement

--- lib/api/auth.dart
+++ lib/api/auth.dart
@@ -16,6 +16,25 @@ class Auth {
     return _toModel(result.user);
   }
 
+  /// 認証リンクをメール送信
+  /// See: https://firebase.google.com/docs/auth/web/email-link-auth?hl=ja#send_an_authentication_link_to_the_users_email_address
+  Future<void> sendSignInLinkToEmail({String url}) async {
+    final user = await currentUser;
+    if (user == null || user.email.isEmpty) return;
+
+    // See: https://firebase.google.com/docs/auth/web/passing-state-in-email-actions
+    // NOTE: 現状 ios, android アプリは考えてない
+    await _auth.sendSignInWithEmailLink(
+      email: user.email,
+      url: url,
+      iOSBundleID: null,
+      handleCodeInApp: null,
+      androidMinimumVersion: null,
+      androidPackageName: null,
+      androidInstallIfNotAvailable: null,
+    );
+  }
+
   /// メールリンク認証
   /// See: https://firebase.google.com/docs/auth/web/email-link-auth
   Future<UserModel> signInWithEmailAndLink({String email, String link}) async {
@@ -23,16 +42,23 @@ class Auth {
     return _toModel(result.user);
   }
 
-  /// 現在ログインしてるユーザ
-  /// オブザーバを使うことで、非ログイン状態のハンドリングをしやすくする
+  /// 現在ログインしてるユーザのオブザーバ
   /// See: https://firebase.google.com/docs/auth/web/manage-users
-  Stream<UserModel> get currentUser => _auth.onAuthStateChanged.asyncMap(_toModel);
+  Stream<UserModel> get currentUserStream => _auth.onAuthStateChanged.asyncMap(_toModel);
+
+  /// 現在ログインしてるユーザ
+  /// See: https://firebase.google.com/docs/auth/web/manage-users
+  Future<UserModel> get currentUser async {
+    final user = await _auth.currentUser();
+    return _toModel(user);
+  }
 
   Future<void> signOut() async => _auth.signOut();
 
   UserModel _toModel(FirebaseUser user) => UserModel(
         id: user.uid,
         name: user.displayName,
+        email: user.email,
         // TODO: 他に必要な属性が無いか検討
       );
 }
