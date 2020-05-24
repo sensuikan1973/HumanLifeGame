9fb2bc78362a630bcbd031b965919e0b1c294cba
TomoyukiSugiyama <u3.gpw25@ezweb.ne.jp>
2020-05-25 01:07:30 +0900

    Decorate life step (#135)
    * add card widget
    
    * delete type nothing
    
    * fix body layout
    
    * delete unused import
    
    * add shadow
    
    * fix LifeStep test
    
    * fix test
    
    * fix human size
    
    * delete unused method
    
    * delete unused var
    
    * remove unused import

--- lib/screens/common/human.dart
+++ lib/screens/common/human.dart
@@ -14,12 +14,21 @@ class Human extends StatelessWidget {
 
   @visibleForTesting
   static const orderedIcon = [
-    Icon(Icons.directions_run, color: Colors.red, size: 20),
-    Icon(Icons.directions_bike, color: Colors.blue, size: 20),
-    Icon(Icons.directions_car, color: Colors.green, size: 20),
-    Icon(Icons.atm, color: Colors.yellow, size: 20),
+    Icon(Icons.directions_run, color: Colors.red, size: 25),
+    Icon(Icons.directions_bike, color: Colors.blue, size: 25),
+    Icon(Icons.directions_car, color: Colors.green, size: 25),
+    Icon(Icons.atm, color: Colors.yellow, size: 25),
   ];
 
+  static const orderedIconShadow = [
+    Icon(Icons.directions_run, color: Colors.black12, size: 25),
+    Icon(Icons.directions_bike, color: Colors.black12, size: 25),
+    Icon(Icons.directions_car, color: Colors.black12, size: 25),
+    Icon(Icons.atm, color: Colors.black12, size: 25),
+  ];
   @override
-  Widget build(BuildContext context) => orderedIcon[_humanModel.order];
+  Widget build(BuildContext context) => Stack(children: <Widget>[
+        Positioned(top: 2, left: 2, child: orderedIconShadow[_humanModel.order]),
+        orderedIcon[_humanModel.order],
+      ]);
 }
