864ac079fd12575fc2d5829dbdd754836cc64c8d
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-25 01:19:45 +0900

    remove unnecessary assignment

--- lib/models/play_room/play_room.dart
+++ lib/models/play_room/play_room.dart
@@ -94,17 +94,15 @@ class PlayRoomNotifier extends ValueNotifier<PlayRoomState> {
     _remainCount = 0; // リセット
 
     // LifeEvent 処理
-    value.lifeStages = [...value.lifeStages];
     value.lifeStages[_currentPlayerLifeStageIndex] = _lifeEventService.executeEvent(
       _currentPlayerLifeStage.lifeStepModel.lifeEvent,
       _currentPlayerLifeStage,
     );
 
     // LifeEventの履歴を更新
-    value.everyLifeEventRecords = [
-      ...value.everyLifeEventRecords,
-      LifeEventRecordModel(_i18n, _currentPlayerLifeStage.human, _currentPlayerLifeStage.lifeStepModel.lifeEvent)
-    ];
+    value.everyLifeEventRecords.add(
+      LifeEventRecordModel(_i18n, _currentPlayerLifeStage.human, _currentPlayerLifeStage.lifeStepModel.lifeEvent),
+    );
 
     _changeToNextTurn(); // FIXME: 即ターン交代してるけど、あくまで仮
   }
