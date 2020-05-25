56f9cb3b3dacf1dd7d79640321a4258dc3284872
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-25 21:59:13 +0900

    load .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'human_life_game_app.dart';
 
 Future<void> main() async {
   // ref: https://flutter.dev/docs/testing/debugging
+
+  await DotEnv().load('config/.env');
 
   runApp(
     HumanLifeGameApp.inProviders(),
