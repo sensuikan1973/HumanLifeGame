624036666c1dce9f13505b7063b26df110c9dd71
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-25 22:17:23 +0900

    check sign in

--- lib/screens/lobby/lobby.dart
+++ lib/screens/lobby/lobby.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../api/auth.dart';
+import '../../models/common/user.dart';
 import '../../router.dart';
 
 /// FIXME: ロジックべた書き
@@ -13,6 +15,14 @@ class Lobby extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              FutureBuilder<UserModel>(
+                future: context.watch<Auth>().currentUser,
+                builder: (context, snap) {
+                  if (snap.hasError) return const Text('Oops');
+                  if (snap.hasData) return Text(snap.data.id);
+                  return const Text('You must sign in');
+                },
+              ),
               RaisedButton(
                 onPressed: () => Navigator.of(context).pushNamed(context.read<Router>().playRoom),
                 child: const Text('Go to PlayRoom'),
