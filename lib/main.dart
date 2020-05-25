cf0ece6ce4f851399eaa45d19f8a8353928ba985
done <sensuikan1973@gmail.com>
2020-05-26 01:13:40 +0900

    fix Web_Deploy action  (#143)
    * DotEnv は別に使わなくても sample ロードしとけばいい。処理統一した方が楽
    
    * fix deploy job

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -7,8 +6,7 @@ import 'human_life_game_app.dart';
 Future<void> main() async {
   // ref: https://flutter.dev/docs/testing/debugging
 
-  // NOTE: 現状 Production Mode では不要
-  if (!kReleaseMode) await DotEnv().load('config/.env');
+  await DotEnv().load('config/.env');
 
   runApp(
     HumanLifeGameApp.inProviders(),
