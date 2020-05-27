7fc8c1a50dff8ca96ee12015fd094602768c64c0
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-05-28 08:05:34 +0900

    remove unused import

--- test/widget/play_view_test.dart
+++ test/widget/play_view_test.dart
@@ -4,7 +4,6 @@ import 'package:HumanLifeGame/models/common/human_life.dart';
 import 'package:HumanLifeGame/models/common/life_event.dart';
 import 'package:HumanLifeGame/models/common/life_event_params/gain_life_items_params.dart';
 import 'package:HumanLifeGame/models/common/life_event_params/goal_params.dart';
-import 'package:HumanLifeGame/models/common/life_event_params/life_event_params.dart';
 import 'package:HumanLifeGame/models/common/life_event_params/nothing_params.dart';
 import 'package:HumanLifeGame/models/common/life_event_params/start_params.dart';
 import 'package:HumanLifeGame/models/common/life_road.dart';
