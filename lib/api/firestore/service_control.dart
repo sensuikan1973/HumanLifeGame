67da50010308c32b29da90790c9cc4c27ef06679
ShimizuNaoki <sensuikan1973@gmail.com>
2020-05-31 00:32:59 +0900

    Store class

--- lib/api/firestore/service_control.dart
+++ lib/api/firestore/service_control.dart
@@ -2,6 +2,8 @@ import 'package:firestore_ref/firestore_ref.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import 'firestore.dart';
+
 part 'service_control.freezed.dart';
 part 'service_control.g.dart';
 
@@ -15,14 +17,16 @@ abstract class ServiceControl with _$ServiceControl {
   factory ServiceControl.fromJson(Map<String, dynamic> json) => _$ServiceControlFromJson(json);
 }
 
-final CollectionRef<ServiceControl, Document<ServiceControl>> serviceControlsRef = CollectionRef(
-  Firestore.instance.collection('serviceControl'),
-  decoder: (snapshot) => Document(snapshot.reference, ServiceControl.fromJson(snapshot.data)),
-  encoder: (serviceControl) => replacingTimestamp(
-    json: serviceControl.toJson(),
-    createdAt: serviceControl.updatedAt,
-  ),
-);
+const _collectionId = 'serviceControl';
+
+CollectionRef<ServiceControl, Document<ServiceControl>> serviceControlsRef(Store store) => CollectionRef(
+      store.firestore.collection(_collectionId),
+      decoder: (snapshot) => Document(snapshot.reference, ServiceControl.fromJson(snapshot.data)),
+      encoder: (serviceControl) => replacingTimestamp(
+        json: serviceControl.toJson(),
+        createdAt: serviceControl.updatedAt,
+      ),
+    );
 
 class ServiceControlField {
   static const isMaintenance = 'isMaintenance';
