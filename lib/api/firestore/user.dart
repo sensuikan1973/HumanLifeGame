d22835e577f3065e634f53882a554e20e57096bd
ShimizuNaoki <sensuikan1973@gmail.com>
2020-06-06 01:50:33 +0900

    User on Firestore

---
+++ lib/api/firestore/user.dart
@@ -0,0 +1,34 @@
+import 'package:firestore_ref/firestore_ref.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+abstract class User with _$User {
+  const factory User({
+    @required String uid,
+    @required String displayName,
+    @TimestampConverter() DateTime createdAt,
+    @TimestampConverter() DateTime updatedAt,
+  }) = _User;
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  @visibleForTesting
+  static const collectionId = 'user';
+}
+
+class UserField {
+  /// uid (by Auth)
+  static const uid = 'uid';
+
+  /// displayName (by Auth)
+  static const displayName = 'displayName';
+
+  /// 作成時刻
+  static const createdAt = 'updatedAt';
+
+  /// 更新時刻
+  static const updatedAt = 'updatedAt';
+}
