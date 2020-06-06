eec5467d3e34397935489ce5b6308fb715c16998
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-06-07 05:02:33 +0900

    add files

--- lib/screens/lobby/lobby.dart
+++ lib/screens/lobby/lobby.dart
@@ -1,11 +1,11 @@
+import 'package:HumanLifeGame/screens/lobby/room_list_item.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
 
 import '../../api/auth.dart';
 import '../../models/common/user.dart';
-import '../../router.dart';
+import 'create_human_life.dart';
 
 /// FIXME: ロジックべた書き
 class Lobby extends StatelessWidget {
@@ -48,89 +48,101 @@ class Lobby extends StatelessWidget {
         body: Center(
           child: Row(
             children: [
-              playView(context),
-              createHumanLifeGame(),
+              roomList(),
+              const CreateHumanLife(),
             ],
           ),
         ),
       );
 
-  SizedBox playView(BuildContext context) {
+  SizedBox roomList() {
     return SizedBox(
       width: 720,
-      height: 1020,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          roomTitle(),
-          Row(
-            children: [
-              roomImage(),
-              Column(
-                children: [
-                  playingHumans(),
-                  FutureBuilder<UserModel>(
-                    future: _signIn(context.watch<Auth>()),
-                    builder: (context, snap) {
-                      if (snap.hasError) return const Text('Oops');
-                      if (snap.connectionState == ConnectionState.waiting) return const Text('waiting...');
-                      if (snap.hasData) return Text(snap.data.id);
-                      return const Text('You must sign in');
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
-          RaisedButton(
-            onPressed: () => Navigator.of(context).pushNamed(context.read<Router>().playRoom),
-            child: const Text('Join the game'),
-          ),
+      height: 970,
+      child: ListView(
+        children: const [
+          RoomListItem(),
+          RoomListItem(),
         ],
       ),
     );
   }
+//  SizedBox playView(BuildContext context) {
+//    return SizedBox(
+//      width: 720,
+//      height: 1020,
+//      child: Column(
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: [
+//          roomTitle(),
+//          Row(
+//            children: [
+//              roomImage(),
+//              Column(
+//                children: [
+//                  playingHumans(),
+//                  FutureBuilder<UserModel>(
+//                    future: _signIn(context.watch<Auth>()),
+//                    builder: (context, snap) {
+//                      if (snap.hasError) return const Text('Oops');
+//                      if (snap.connectionState == ConnectionState.waiting) return const Text('waiting...');
+//                      if (snap.hasData) return Text(snap.data.id);
+//                      return const Text('You must sign in');
+//                    },
+//                  ),
+//                ],
+//              ),
+//            ],
+//          ),
+//          RaisedButton(
+//            onPressed: () => Navigator.of(context).pushNamed(context.read<Router>().playRoom),
+//            child: const Text('Join the game'),
+//          ),
+//        ],
+//      ),
+//    );
+//  }
 
-  SizedBox roomTitle() {
-    return SizedBox(
-      width: 720,
-      height: 40,
-      child: ColoredBox(
-        color: Colors.grey[100],
-        child: const Text('Room 1'),
-      ),
-    );
-  }
+//  SizedBox roomTitle() {
+//    return SizedBox(
+//      width: 720,
+//      height: 40,
+//      child: ColoredBox(
+//        color: Colors.grey[100],
+//        child: const Text('Room 1'),
+//      ),
+//    );
+//  }
 
-  SizedBox roomImage() {
-    return SizedBox(
-      width: 300,
-      height: 200,
-      child: Image.asset(
-        'images/play_view_background.jpg',
-        fit: BoxFit.fitHeight,
-      ),
-    );
-  }
+//  SizedBox roomImage() {
+//    return SizedBox(
+//      width: 300,
+//      height: 200,
+//      child: Image.asset(
+//        'images/play_view_background.jpg',
+//        fit: BoxFit.fitHeight,
+//      ),
+//    );
+//  }
 
-  SizedBox playingHumans() {
-    return SizedBox(
-      width: 420,
-      height: 170,
-      child: Column(
-        children: [
-          const Text('Human 1'),
-          const Text('Human 2'),
-        ],
-      ),
-    );
-  }
+//  SizedBox playingHumans() {
+//    return SizedBox(
+//      width: 420,
+//      height: 170,
+//      child: Column(
+//        children: const [
+//          Text('Human 1'),
+//          Text('Human 2'),
+//        ],
+//      ),
+//    );
+//  }
 
-  SizedBox createHumanLifeGame() {
-    return const SizedBox(
-      width: 720,
-      height: 1020,
-      child: Center(child: Text('開発中')),
-    );
-  }
+//  SizedBox createHumanLifeGame() {
+//    return const SizedBox(
+//      width: 720,
+//      height: 1020,
+//      child: Center(child: Text('開発中')),
+//    );
+//  }
 }
---
+++ lib/screens/lobby/room_list_item.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../../router.dart';
+
+class RoomListItem extends StatelessWidget {
+  const RoomListItem({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        children: [
+          roomTitle(),
+          Row(
+            children: [
+              preview(),
+              Column(
+                children: [
+                  humans(),
+                  joinButton(context),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  SizedBox roomTitle() {
+    return SizedBox(
+      width: 720,
+      height: 40,
+      child: ColoredBox(
+        color: Colors.grey[100],
+        child: const Text('Room 1'),
+      ),
+    );
+  }
+
+  SizedBox preview() {
+    return SizedBox(
+      width: 300,
+      height: 200,
+      child: Image.asset(
+        'images/play_view_background.jpg',
+        fit: BoxFit.fitHeight,
+      ),
+    );
+  }
+
+  SizedBox humans() {
+    return SizedBox(
+      width: 420,
+      height: 170,
+      child: Column(
+        children: const [
+          Text('Human 1'),
+          Text('Human 2'),
+        ],
+      ),
+    );
+  }
+
+  SizedBox joinButton(BuildContext context) {
+    return SizedBox(
+      width: 420,
+      height: 40,
+      child: FlatButton(
+        onPressed: null, //() => Navigator.of(context).pushNamed(context.read<Router>().playRoom),
+        child: const Text('Join a Game'),
+      ),
+    );
+  }
+}
