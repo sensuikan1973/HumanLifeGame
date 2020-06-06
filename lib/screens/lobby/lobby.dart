bb5bb3a31bd04156d3377942100448066da5f90c
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-06-06 21:29:39 +0900

    memo

--- lib/screens/lobby/lobby.dart
+++ lib/screens/lobby/lobby.dart
@@ -41,25 +41,96 @@ class Lobby extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: AppBar(title: const Text('Human Life Game')),
+        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
             children: [
-              FutureBuilder<UserModel>(
-                future: _signIn(context.watch<Auth>()),
-                builder: (context, snap) {
-                  if (snap.hasError) return const Text('Oops');
-                  if (snap.connectionState == ConnectionState.waiting) return const Text('waiting...');
-                  if (snap.hasData) return Text(snap.data.id);
-                  return const Text('You must sign in');
-                },
-              ),
-              RaisedButton(
-                onPressed: () => Navigator.of(context).pushNamed(context.read<Router>().playRoom),
-                child: const Text('Go to PlayRoom'),
-              ),
+              playView(context),
+              createHumanLifeGame(),
             ],
           ),
         ),
       );
+
+  SizedBox playView(BuildContext context) {
+    return SizedBox(
+      width: 720,
+      height: 1020,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          roomTitle(),
+          Row(
+            children: [
+              roomImage(),
+              Column(
+                children: [
+                  playingHumans(),
+                  FutureBuilder<UserModel>(
+                    future: _signIn(context.watch<Auth>()),
+                    builder: (context, snap) {
+                      if (snap.hasError) return const Text('Oops');
+                      if (snap.connectionState == ConnectionState.waiting) return const Text('waiting...');
+                      if (snap.hasData) return Text(snap.data.id);
+                      return const Text('You must sign in');
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          RaisedButton(
+            onPressed: () => Navigator.of(context).pushNamed(context.read<Router>().playRoom),
+            child: const Text('Join the game'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SizedBox roomTitle() {
+    return SizedBox(
+      width: 720,
+      height: 40,
+      child: ColoredBox(
+        color: Colors.grey[100],
+        child: const Text('Room 1'),
+      ),
+    );
+  }
+
+  SizedBox roomImage() {
+    return SizedBox(
+      width: 300,
+      height: 200,
+      child: Image.asset(
+        'images/play_view_background.jpg',
+        fit: BoxFit.fitHeight,
+      ),
+    );
+  }
+
+  SizedBox playingHumans() {
+    return SizedBox(
+      width: 420,
+      height: 170,
+      child: Column(
+        children: [
+          const Text('Human 1'),
+          const Text('Human 2'),
+        ],
+      ),
+    );
+  }
+
+  SizedBox createHumanLifeGame() {
+    return const SizedBox(
+      width: 720,
+      height: 1020,
+      child: Center(child: Text('開発中')),
+    );
+  }
 }
