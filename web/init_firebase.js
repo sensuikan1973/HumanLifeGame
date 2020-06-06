910acb22ec0d5fbf48ea5d62f937a34e03984b30
done <23427957+sensuikan1973@users.noreply.github.com>
2020-06-06 23:58:03 +0900

    refactor PlayRoom logic (#168)
    * remove unnecessary notify
    
    * refactor js
    
    * remove incorrect assert
    
    * indent
    
    * add comment
    
    * remove tmp coment
    
    * separate method
    
    * add comment

--- web/init_firebase.js
+++ web/init_firebase.js
@@ -1,14 +1,15 @@
 if(window.location.hostname === 'localhost'){
- firebase.initializeApp({
-    apiKey: "AIzaSyC__1zDF8-3wqj6-6srMTkNbRPYQzD52B4",
-    appId: "1:273276530293:web:fcadec7fa9ba2f159ad23c",
-    authDomain: "human-life-game-dev.firebaseapp.com",
-    databaseURL: "https://human-life-game-dev.firebaseio.com",
-    measurementId: "G-P10ZHWT57Q",
-    messagingSenderId: "273276530293",
-    projectId: "human-life-game-dev",
-    storageBucket: "human-life-game-dev.appspot.com"
- });
+  const firebaseConfig = {
+     apiKey: "AIzaSyC__1zDF8-3wqj6-6srMTkNbRPYQzD52B4",
+     authDomain: "human-life-game-dev.firebaseapp.com",
+     databaseURL: "https://human-life-game-dev.firebaseio.com",
+     projectId: "human-life-game-dev",
+     storageBucket: "human-life-game-dev.appspot.com",
+     messagingSenderId: "273276530293",
+     appId: "1:273276530293:web:fcadec7fa9ba2f159ad23c",
+     measurementId: "G-P10ZHWT57Q"
+  };
+  firebase.initializeApp(firebaseConfig);
 } else {
   fetch("/__/firebase/init.json").then( res => res.json() ).then( json => {
    firebase.initializeApp(json);
