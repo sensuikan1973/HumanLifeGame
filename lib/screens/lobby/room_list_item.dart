e9f510ec400be4655d1925cc45b0b3e8d6db6074
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-06-07 17:13:05 +0900

    convert to expression body

--- lib/screens/lobby/room_list_item.dart
+++ lib/screens/lobby/room_list_item.dart
@@ -7,61 +7,53 @@ class RoomListItem extends StatelessWidget {
   const RoomListItem({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Column(
-        children: [
-          _roomTitle(),
-          Row(
-            children: [
-              _preview(),
-              Column(
-                children: [
-                  _humans(),
-                  _joinButton(context),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => SizedBox(
+        child: Column(
+          children: [
+            _roomTitle(),
+            Row(
+              children: [
+                _preview(),
+                Column(
+                  children: [
+                    _humans(),
+                    _joinButton(context),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
 
-  SizedBox _roomTitle() {
-    return SizedBox(
-      width: 720,
-      height: 40,
-      child: ColoredBox(
-        color: Colors.grey[100],
-        child: const Text('Room 1'),
-      ),
-    );
-  }
+  SizedBox _roomTitle() => SizedBox(
+        width: 720,
+        height: 40,
+        child: ColoredBox(
+          color: Colors.grey[100],
+          child: const Text('Room 1'),
+        ),
+      );
 
-  SizedBox _preview() {
-    return SizedBox(
-      width: 300,
-      height: 200,
-      child: Image.asset(
-        'images/play_view_background.jpg',
-        fit: BoxFit.fitHeight,
-      ),
-    );
-  }
+  SizedBox _preview() => SizedBox(
+        width: 300,
+        height: 200,
+        child: Image.asset(
+          'images/play_view_background.jpg',
+          fit: BoxFit.fitHeight,
+        ),
+      );
 
-  SizedBox _humans() {
-    return SizedBox(
-      width: 420,
-      height: 170,
-      child: Column(
-        children: const [
-          Text('Human 1'),
-          Text('Human 2'),
-        ],
-      ),
-    );
-  }
+  SizedBox _humans() => SizedBox(
+        width: 420,
+        height: 170,
+        child: Column(
+          children: const [
+            Text('Human 1'),
+            Text('Human 2'),
+          ],
+        ),
+      );
 
   SizedBox _joinButton(BuildContext context) => SizedBox(
         width: 420,
