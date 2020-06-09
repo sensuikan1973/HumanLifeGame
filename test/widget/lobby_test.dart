f05a88e4d8581946253d2bda942c45664f60a246
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2020-06-10 00:10:48 +0900

    add MockAuth

--- test/widget/lobby_test.dart
+++ test/widget/lobby_test.dart
@@ -2,6 +2,7 @@ import 'package:HumanLifeGame/api/auth.dart';
 import 'package:HumanLifeGame/models/common/user.dart';
 import 'package:HumanLifeGame/screens/lobby/create_human_life.dart';
 import 'package:HumanLifeGame/screens/lobby/lobby.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
@@ -10,10 +11,15 @@ import '../mocks/auth.dart';
 import 'helper/widget_build_helper.dart';
 
 Future<void> main() async {
+  setUp(() {
+    // See: https://github.com/flutter/flutter/issues/12994#issuecomment-397321431
+    // Desktopの標準サイズ 1440x1024に設定
+    WidgetsBinding.instance.renderView.configuration = TestViewConfiguration(size: const Size(1440, 1024));
+  });
   testWidgets('show some widgets', (tester) async {
     final user = UserModel(id: '123', name: 'foo', isAnonymous: true);
     final auth = MockAuth();
-    when(auth.currentUserStream).thenAnswer((_) => Stream.fromIterable([user]));
+    when(auth.currentUser).thenAnswer((_) async => user);
     await tester.pumpWidget(testableApp(
       home: Provider<Auth>(create: (_) => auth, child: const Lobby()),
     ));
