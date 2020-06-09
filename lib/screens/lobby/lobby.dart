d4ff2f326a039f60c0a185efa8226349a36b6b07
ShimizuNaoki <sensuikan1973@gmail.com>
2020-06-10 01:38:08 +0900

    fix Sign in logic on DebugMode

--- lib/screens/lobby/lobby.dart
+++ lib/screens/lobby/lobby.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 import '../../api/auth.dart';
@@ -12,32 +11,11 @@ import 'room_list_item.dart';
 class Lobby extends StatelessWidget {
   const Lobby({Key key}) : super(key: key);
 
-  /// Only for Debug Mode
-  Future<UserModel> _signInOrCreateWithEmailAndPassForDev(Auth auth) async {
-    final env = DotEnv().env;
-    final email = env['EMAIL'] ?? '';
-    final pass = env['PASS'] ?? '';
-    if (email.isEmpty || pass.isEmpty) return null;
-    UserModel user;
-    try {
-      user = await auth.createUserWithEmailAndPassword(email: email, password: pass);
-      // ignore: avoid_catches_without_on_clauses
-    } catch (e) {
-      // See: https://github.com/FirebaseExtended/flutterfire/pull/1698
-      // 上記 issue にあるように、現状 Error を広く拾って強引に解釈する他ない
-      final code = e.code as String;
-      if (code == 'auth/email-already-in-use') {
-        user = await auth.signInWithEmailAndPassword(email: email, password: pass);
-      }
-    }
-    return user;
-  }
-
   Future<UserModel> _signIn(Auth auth) async {
-    var user = await auth.currentUser;
+    final user = await auth.currentUser;
     if (user != null) return user;
-    if (!kReleaseMode) user = await _signInOrCreateWithEmailAndPassForDev(auth);
-    return user ?? await auth.signInAnonymously();
+    if (kDebugMode) return auth.signInForDebug();
+    return auth.signInAnonymously();
   }
 
   @override
