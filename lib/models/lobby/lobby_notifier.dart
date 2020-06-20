8a5c21a924485b6a0e5575d8501de42ea1a5bdbc
ShimizuNaoki <sensuikan1973@gmail.com>
2020-06-20 22:51:41 +0900

    fix

--- lib/models/lobby/lobby_notifier.dart
+++ lib/models/lobby/lobby_notifier.dart
@@ -1,9 +1,3 @@
-import 'package:firestore_ref/firestore_ref.dart';
-
 class LobbyNotifier {
-  LobbyNotifier(this._firestore);
-
-  final Firestore _firestore;
-
-  Future<void> createPlayRoom() async {}
+  LobbyNotifier();
 }
