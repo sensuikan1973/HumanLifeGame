f4f43912c712e6d3336c889603cb3c9cbabffaa3
ShimizuNaoki <sensuikan1973@gmail.com>
2020-06-22 00:41:04 +0900

    tmp fetch play rooms

--- lib/models/lobby/lobby_state.dart
+++ lib/models/lobby/lobby_state.dart
@@ -1,3 +1,9 @@
+import 'package:firestore_ref/firestore_ref.dart';
+
+import '../../api/firestore/play_room.dart';
+
 class LobbyState {
   LobbyState();
+
+  List<Document<PlayRoom>> publicPlayRooms = [];
 }
