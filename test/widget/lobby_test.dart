a50590a81fbc619dea6f2cc84e71582e0b57843d
ShimizuNaoki <sensuikan1973@gmail.com>
2020-06-27 04:34:46 +0900

    change test helper

--- test/widget/lobby_test.dart
+++ test/widget/lobby_test.dart
@@ -1,13 +1,10 @@
 import 'dart:math';
 
-import 'package:HumanLifeGame/api/auth.dart';
-import 'package:HumanLifeGame/api/dice.dart';
 import 'package:HumanLifeGame/api/firestore/life_road.dart';
 import 'package:HumanLifeGame/api/firestore/play_room.dart';
 import 'package:HumanLifeGame/api/firestore/store.dart';
 import 'package:HumanLifeGame/api/firestore/user.dart';
 import 'package:HumanLifeGame/i18n/i18n.dart';
-import 'package:HumanLifeGame/router.dart';
 import 'package:HumanLifeGame/screens/lobby/human_life_tips.dart';
 import 'package:HumanLifeGame/screens/lobby/lobby.dart';
 import 'package:HumanLifeGame/screens/lobby/room_list_item.dart';
@@ -16,7 +13,6 @@ import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:provider/provider.dart';
 
 import '../mocks/auth.dart';
 import 'helper/widget_build_helper.dart';
@@ -37,15 +33,7 @@ Future<void> main() async {
     final firestore = MockFirestoreInstance();
     final store = Store(firestore);
     await tester.pumpWidget(
-      testableApp(
-        providers: [
-          Provider<Router>(create: (_) => const Router()),
-          Provider<Dice>(create: (_) => const Dice()),
-          Provider<Auth>(create: (_) => auth),
-          Provider<Store>(create: (_) => store),
-        ],
-        home: Lobby.inProviders(),
-      ),
+      testableApp(auth: auth, store: store, home: Lobby.inProviders()),
     );
     await tester.pump();
     await tester.pump();
@@ -89,15 +77,7 @@ Future<void> main() async {
     }
 
     await tester.pumpWidget(
-      testableApp(
-        providers: [
-          Provider<Router>(create: (_) => const Router()),
-          Provider<Dice>(create: (_) => const Dice()),
-          Provider<Auth>(create: (_) => auth),
-          Provider<Store>(create: (_) => store),
-        ],
-        home: Lobby.inProviders(),
-      ),
+      testableApp(auth: auth, store: store, home: Lobby.inProviders()),
     );
     await tester.pump();
     await tester.pump();
