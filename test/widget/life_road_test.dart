c4b0efa8370988e0c560e2f3ce6da23a7724f82a
ShimizuNaoki <sensuikan1973@gmail.com>
2020-07-08 00:56:55 +0900

    fix life road widget test

--- test/widget/life_road_test.dart
+++ test/widget/life_road_test.dart
@@ -1,5 +1,4 @@
 import 'package:HumanLifeGame/api/firestore/life_event.dart';
-import 'package:HumanLifeGame/api/firestore/life_road.dart';
 import 'package:HumanLifeGame/api/firestore/store.dart';
 import 'package:HumanLifeGame/human_life_game_app.dart';
 import 'package:HumanLifeGame/i18n/i18n.dart';
@@ -12,15 +11,14 @@ import 'package:HumanLifeGame/models/play_room/play_room_notifier.dart';
 import 'package:HumanLifeGame/screens/common/life_road.dart';
 import 'package:HumanLifeGame/screens/common/life_step.dart';
 import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
+import '../helper/firestore/life_road_helper.dart';
 import '../helper/firestore/play_room_helper.dart';
 import '../mocks/dice.dart';
 import 'helper/testable_app.dart';
 
-// FIXME:
 Future<void> main() async {
   final i18n = await I18n.load(HumanLifeGameApp.defaultLocale);
   const start = LifeEventEntity<StartParams>(
@@ -52,17 +50,16 @@ Future<void> main() async {
     [blank, blank, blank, blank, blank, blank, blank],
     [blank, blank, blank, blank, blank, blank, blank],
   ];
-  final lifeRoad = LifeRoadModel(lifeStepsOnBoard: LifeRoadEntity.createLifeStepsOnBoard(lifeEvents));
-  final lifeRoadSize = Size(150.0 * lifeRoad.width, 100.0 * lifeRoad.height);
 
   testWidgets('show LifeSteps', (tester) async {
     final firestore = MockFirestoreInstance();
     final store = Store(firestore);
+    final lifeRoad = await createLifeRoad(store, lifeEvents: lifeEvents);
     final playRoomNotifier = PlayRoomNotifier(i18n, MockDice(), store, await createPlayRoom(store));
     await tester.pumpWidget(testableApp(
       home: ChangeNotifierProvider(
         create: (_) => playRoomNotifier,
-        child: LifeRoad(lifeRoad, lifeRoadSize.width, lifeRoadSize.height),
+        child: LifeRoad(lifeRoad.entity, 150.0 * lifeRoad.entity.width, 100.0 * lifeRoad.entity.height),
       ),
     ));
     await tester.pump();
