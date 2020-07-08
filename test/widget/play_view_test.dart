c6ad659baa05fd72895e94e670cc93e3cb9bb8ca
ShimizuNaoki <sensuikan1973@gmail.com>
2020-07-09 00:22:56 +0900

    enable play view widget test

--- test/widget/play_view_test.dart
+++ test/widget/play_view_test.dart
@@ -1,78 +1,99 @@
-void main() {}
-//import 'package:HumanLifeGame/i18n/i18n.dart';
-//import 'package:HumanLifeGame/models/common/human.dart';
-//import 'package:HumanLifeGame/models/common/life_event.dart';
-//import 'package:HumanLifeGame/models/common/life_event_params/gain_life_items_params.dart';
-//import 'package:HumanLifeGame/models/common/life_event_params/goal_params.dart';
-//import 'package:HumanLifeGame/models/common/life_event_params/nothing_params.dart';
-//import 'package:HumanLifeGame/models/common/life_event_params/start_params.dart';
-//import 'package:HumanLifeGame/models/common/life_road.dart';
-//import 'package:HumanLifeGame/models/play_room/play_room_notifier.dart';
-//import 'package:HumanLifeGame/screens/common/life_step_entity.dart';
-//import 'package:HumanLifeGame/screens/play_room/play_view.dart';
-//import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
-//import 'package:provider/provider.dart';
-//
-//import '../mocks/dice.dart';
-//import 'helper/testable_app.dart';
-//
-//Future<void> main() async {
-//  final i18n = await I18n.load(const Locale('en', 'US'));
-//  final dice = MockDice();
-//  final humans = [const HumanModel(id: 'h1', name: 'foo', order: 0), const HumanModel(id: 'h2', name: 'bar', order: 1)];
-//  final start = LifeEventModel(LifeEventTarget.myself, const StartParams());
-//  final goals = LifeEventModel(LifeEventTarget.myself, const GoalParams());
-//  final gains = LifeEventModel(LifeEventTarget.myself, const GainLifeItemsParams(targetItems: []));
-//  final blank = LifeEventModel(LifeEventTarget.myself, const NothingParams());
-//  final lifeEvents = [
-//    [start, gains, gains, gains, gains, gains, goals],
-//    [blank, blank, blank, blank, blank, blank, blank],
-//    [blank, blank, blank, blank, blank, blank, blank],
-//    [blank, blank, blank, blank, blank, blank, blank],
-//    [blank, blank, blank, blank, blank, blank, blank],
-//    [blank, blank, blank, blank, blank, blank, blank],
-//    [blank, blank, blank, blank, blank, blank, blank],
-//  ];
-//  final lifeRoad = LifeRoadModel(lifeStepsOnBoard: LifeRoadModel.createLifeStepsOnBoard(lifeEvents));
-//
-//  testWidgets('show LifeSteps, display size = (1440, 1024)', (tester) async {
-//    const windowWidth = 1440.0;
-//    const windowHeight = 1024.0;
-//    const size = Size(windowWidth, windowHeight);
-//    final playRoomModel = PlayRoomNotifier(i18n, dice, lifeRoad, humans);
-//
-//    await tester.pumpWidget(
-//      testableApp(
-//        home: ChangeNotifierProvider(
-//          create: (_) => playRoomModel,
-//          child: const MediaQuery(data: MediaQueryData(size: size), child: PlayView()),
-//        ),
-//      ),
-//    );
-//    await tester.pump();
-//
-//    expect(find.byType(LifeStep), findsNWidgets(lifeEvents.expand((el) => el.where((el) => el != blank)).length));
-//  });
-//  testWidgets('show LifeSteps, display size = (1050, 750)', (tester) async {
-//    const windowWidth = 1050.0;
-//    const windowHeight = 750.0;
-//    const size = Size(windowWidth, windowHeight);
-//    final playRoomModel = PlayRoomNotifier(i18n, dice, lifeRoad, humans);
-//
-//    await tester.pumpWidget(
-//      testableApp(
-//        home: ChangeNotifierProvider(
-//          create: (_) => playRoomModel,
-//          child: const MediaQuery(
-//            data: MediaQueryData(size: size),
-//            child: PlayView(),
-//          ),
-//        ),
-//      ),
-//    );
-//    await tester.pump();
-//
-//    expect(find.byType(LifeStep), findsNWidgets(lifeEvents.expand((el) => el.where((el) => el != blank)).length));
-//  });
-//}
+import 'package:HumanLifeGame/api/firestore/store.dart';
+import 'package:HumanLifeGame/human_life_game_app.dart';
+import 'package:HumanLifeGame/i18n/i18n.dart';
+import 'package:HumanLifeGame/models/play_room/play_room_notifier.dart';
+import 'package:HumanLifeGame/screens/common/life_step.dart';
+import 'package:HumanLifeGame/screens/play_room/play_view.dart';
+import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+import '../helper/firestore/life_event_helper.dart';
+import '../helper/firestore/life_road_helper.dart';
+import '../helper/firestore/play_room_helper.dart';
+import '../helper/firestore/user_helper.dart';
+import '../mocks/auth.dart';
+import '../mocks/dice.dart';
+import 'helper/testable_app.dart';
+
+Future<void> main() async {
+  final i18n = await I18n.load(HumanLifeGameApp.defaultLocale);
+  final user = MockFirebaseUser();
+
+  final lifeEvents = [
+    [startEvent, gainEvent, gainEvent, gainEvent, gainEvent, gainEvent, goalEvent],
+    [blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent],
+    [blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent],
+    [blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent],
+    [blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent],
+    [blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent],
+    [blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent, blankEvent],
+  ];
+
+  testWidgets('show LifeSteps, display size = (1440, 1024)', (tester) async {
+    final store = Store(MockFirestoreInstance());
+    final humans = [await createUser(store), await createUser(store, uid: user.uid)];
+    const size = Size(1440, 1024);
+    final lifeRoad = await createLifeRoad(store, lifeEvents: lifeEvents);
+    final playRoomNotifier = PlayRoomNotifier(
+      i18n,
+      MockDice(),
+      store,
+      await createPlayRoom(
+        store,
+        lifeRoad: lifeRoad.ref,
+        host: humans.first.ref,
+        humans: humans.map((el) => el.ref).toList(),
+      ),
+    );
+    await playRoomNotifier.init();
+
+    await tester.pumpWidget(
+      testableApp(
+        home: ChangeNotifierProvider(
+          create: (_) => playRoomNotifier,
+          child: const MediaQuery(data: MediaQueryData(size: size), child: PlayView()),
+        ),
+      ),
+    );
+    await tester.pump();
+    expect(
+      find.byType(LifeStep),
+      findsNWidgets(lifeEvents.expand((el) => el.where((el) => el != blankEvent)).length),
+    );
+  });
+
+  testWidgets('show LifeSteps, display size = (1050, 750)', (tester) async {
+    final store = Store(MockFirestoreInstance());
+    final humans = [await createUser(store), await createUser(store, uid: user.uid)];
+    const size = Size(1050, 750);
+    final lifeRoad = await createLifeRoad(store, lifeEvents: lifeEvents);
+    final playRoomNotifier = PlayRoomNotifier(
+      i18n,
+      MockDice(),
+      store,
+      await createPlayRoom(
+        store,
+        lifeRoad: lifeRoad.ref,
+        host: humans.first.ref,
+        humans: humans.map((el) => el.ref).toList(),
+      ),
+    );
+    await playRoomNotifier.init();
+
+    await tester.pumpWidget(
+      testableApp(
+        home: ChangeNotifierProvider(
+          create: (_) => playRoomNotifier,
+          child: const MediaQuery(data: MediaQueryData(size: size), child: PlayView()),
+        ),
+      ),
+    );
+    await tester.pump();
+    expect(
+      find.byType(LifeStep),
+      findsNWidgets(lifeEvents.expand((el) => el.where((el) => el != blankEvent)).length),
+    );
+  });
+}
